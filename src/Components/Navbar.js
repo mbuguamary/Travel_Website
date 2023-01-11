f95cf680fafbcdb81ff08eb6896df8601e9d9a17
mbuguamary <mary.wairimu01@student.moringaschool.com>
2023-01-11 12:55:04 +0300

    feat: created the navbar page

---
+++ src/Components/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <div className='main'>
+         <ul>
+            <li>Home</li>
+            <li>Services</li>
+            <li>Gallery</li>
+            <li>About</li>
+            <li>Contact</li>
+         </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
