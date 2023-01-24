25c500d56d16f90063ecf0e8c3221ccd612c575a
mbuguamary <mary.wairimu01@student.moringaschool.com>
2023-01-24 23:32:09 +0300

    Feat: adds routes to index.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Gallery from './Components/Gallery';
 import reportWebVitals from './reportWebVitals';
-
+import {BrowserRouter, Routes,Route } from 'react-router-dom'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+    <Routes>
+    <Route path ="/" element={<App/>} />
+    <Route path = "/Gallery" element={<Gallery/>}/>
+     
+    </Routes>
+    </BrowserRouter>
+    
+
   </React.StrictMode>
 );
 
