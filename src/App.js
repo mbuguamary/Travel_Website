358daa547087ee9ae8ef0a8878311a88e5c276de
mbuguamary <mary.wairimu01@student.moringaschool.com>
2023-01-24 21:13:22 +0300

    fix: removed image styling

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ function App() {
   const myStyle={
     backgroundImage: `url(${background})` ,
     height:'100vh',
-    marginTop:'-70px',
-    fontSize:'50px',
+    // marginTop:'-70px',
+    // fontSize:'50px',
     backgroundSize: 'cover',
     backgroundRepeat: 'no-repeat',
 };
