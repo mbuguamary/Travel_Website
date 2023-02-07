e11ec7bf14976741f2c8ed8c34c0aa44ecc1ce27
Mary Mbugua <mbuguamels@gmail.com>
2023-02-07 12:10:05 +0300

    added booking information

--- src/Components/Gallery.js
+++ src/Components/Gallery.js
@@ -15,34 +15,52 @@ const Gallery = () => {
       <div>
         <img src={hiking} alt='Hiking'/>
         <h1>Hiking</h1>
-        <p></p>
+        <h4>Mon Fri</h4>
+        <h5>3hrs</h5>
+        <h5>$65</h5>
+        <button>Book Now</button>
       </div>
       <div>
         <img src={boatriding} alt='Boatriding'/>
         <h1>Boatriding</h1>
-        <p></p>
+        <h4>Mon Fri</h4>
+        <h5>3hrs</h5>
+        <h5>$50</h5>
+        <button>Book Now</button>
       </div>
       <div>
         <img src={camping} alt='Camping'/>
         <h1>Camping</h1>
-        <p></p>
+        <h4>Mon Fri</h4>
+        <h5>3hrs</h5>
+        <h5>$55</h5>
+        <button>Book Now</button>
       </div>
       </div>
       <div className='div2'>
       <div>
         <img src={snorkelling} alt='Snorkelling'/>
-        <h1>snorkelling</h1>
-        <p></p>
+        <h1>Snorkelling</h1>
+        <h4>Mon Fri</h4>
+        <h5>3hrs</h5>
+        <h5>$30</h5>
+        <button>Book Now</button>
       </div>
       <div>
       <img src={bonfire} alt='bonfire'/>
         <h1>Bonfire</h1>
-        <p></p>
+        <h4>Mon Fri</h4>
+        <h5>3hrs</h5>
+        <h5>$55</h5>
+        <button>Book Now</button>
       </div>
       <div>
       <img src={kayaking} alt='Kayaking'/>
         <h1>Kayaking</h1>
-        <p></p>
+        <h4>Mon Fri</h4>
+        <h5>3hrs</h5>
+        <h5>$45</h5>
+        <button>Book Now</button>
       </div>
       </div>
       </div>
