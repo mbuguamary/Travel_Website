265ef6051d938d8d16c4d32964a91ec50fd1376b
Mary Mbugua <mbuguamels@gmail.com>
2023-02-13 08:05:09 +0300

    created the contact page

---
+++ src/Components/Contact.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div className='contact'>
+       <input placeholder='name'/>
+       <input placeholder='email'/>
+       <input placeholder='subject'/>
+       <textarea placeholder='message'/>
+       <button>Submit</button>
+        
+        </div>
+  )
+}
+
+export default Contact
